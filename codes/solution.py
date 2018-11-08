111bfa275a807889a73567378bf8100c419de2aa
yhz012 <603907639@qq.com>
2018-11-08 15:09:41 +0800

    easiest case done!

--- codes/solution.py
+++ codes/solution.py
@@ -3,31 +3,123 @@ import numpy as np
 import frame
 
 class player(object):
-	def __init__(self, board):
-		#frame.board board: game board
-		self.b = board
-		self.reportHistory
+    def __init__(self, board, quickRes = False, double = False):
+        #frame.board board: game board
+        self.b = board
+        self.reportHistory = []
+        self.quickRes = quickRes
+        self.double = double
 
-		self.history = []
-		return
-	
-	#update b.prob after search
-	def updateP(self, row, col):
-		return
+        self.success = False
+        self.history = []
+        return
+    
+    #update b.prob after search
+    def updateP(self, row, col, quick = False, force = False, temp = False):
+        if temp:
+            tempProb = np.copy(self.b.prob)
+        else:
+            tempProb = self.b.prob
 
-	#get next block to search
-	def getNext(self, row = None, col = None):
-		if row is None or col is None:
-			#get next promising block
-			return 
-		else:
-			candidate = self.b.getNeighbor(*self.b.hunter)
-			#move to candidate or explore here
-			#if move to candidate, which one to move
-			return
+        tempProb[row, col] = tempProb[row, col] * self.b.failP[self.b.cell[row, col]]
+        sumP = np.sum(tempProb)
 
-	def solve(self):
-		#loop
-			#getNext
-			#updateP
-		return
+        if not force and (self.quickRes or quick) and sumP > 0.5:
+            return
+        tempProb = self.normalizeP(tempProb, sumP)
+        if not temp:
+            self.b.prob = tempProb
+        return tempProb
+
+    #get next block to search
+    def getNext(self, row = None, col = None, rule = 3):
+        if rule == 1:
+            pos = self.maxProb(row, col)
+        elif rule == 2:
+            pos = self.maxSucP(row, col)
+        elif rule == 3:
+            pos = self.maxInfo(row, col)
+        else:
+            print('E: solution.getNext. wrong rule number.')
+        return pos
+
+    #tool functions
+    #resize prob so that sum == 1
+    def normalizeP(self, tempProb, sumP = None):
+        if sumP is None:
+            sumP = np.sum(tempProb)
+        tempProb = tempProb / sumP
+        return tempProb
+
+    #
+    def maxProb(self, row = None, col = None):
+        if row is None or col is None:
+            value = self.b.prob
+            pos = np.unravel_index(np.argmax(value), value.shape)
+            return pos
+        else:
+            candidate = self.b.getNeighbor(*self.b.hunter)
+            #move to candidate or explore here
+            #if move to candidate, which one to move
+            return
+
+    def maxSucP(self, row = None, col = None):
+        if row is None or col is None:
+            value = self.b.prob * self.b.sucP
+            pos = np.unravel_index(np.argmax(value), value.shape)
+            return pos
+        else:
+            candidate = self.b.getNeighbor(*self.b.hunter)
+            #move to candidate or explore here
+            #if move to candidate, which one to move
+            return
+
+    def maxInfo(self, row = None, col = None):
+        if row is None or col is None:
+            tempSucP = self.b.prob * self.b.sucP
+            tempSucQ = 1 - tempSucP
+            searchInfo = tempSucP * np.log2(tempSucP) + tempSucQ * np.log2(tempSucQ) #there should be negative. use min to find max
+            if self.b.targetMoving:
+                pass #TODO: value = searchreportInfo
+            else:
+                value = searchInfo
+            pos = np.unravel_index(np.argmin(value), value.shape) #use min instead
+            return pos
+        else:
+            candidate = self.b.getNeighbor(*self.b.hunter)
+            #move to candidate or explore here
+            #if move to candidate, which one to move
+            return
+
+    def solve(self):
+        while not self.success:
+            pos = self.getNext(rule = 1)
+
+            iterNum = 1
+            if self.double and self.b.cell[pos] < self.double:
+                iterNum = iterNum + 1
+
+            for i in range(iterNum):
+                self.success, report = self.b.explore(*pos)
+                self.history.append((pos, 's'))
+
+                if self.success:
+                    break
+                self.updateP(*pos)
+
+            if len(self.history) > 100000:
+                break
+
+            # print(self.b.prob)
+            # self.b.visualize()
+        return
+
+if __name__ == '__main__':
+    b = frame.board(size = 50)
+    p = player(b, double = 2)
+    p.solve()
+
+    print(len(p.history))
+    print(p.b.prob[p.b._target])
+    print(np.max(p.b.prob))
+    p.b.visualize()
